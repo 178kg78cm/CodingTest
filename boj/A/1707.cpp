62295c3aaaf24057c1d0be7830733e7f5f968251
178kg78cm <jhh7550@gmail.com>
2025-01-24 17:46:21 +0900

    0124

---
+++ boj/A/1707.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        int V,E;
+        cin >> V >> E;
+        vector<int> graph[V];
+        for(int i=0;i<E;i++){
+            int u, v;
+            cin >> u >> v;
+            graph[u].push_back(v);
+            graph[v].push_back(u);
+        }
+
+        for(int i=0;i<V;i++)
+
+    }
+
+
+
+}
--- boj/A/2589.cpp
+++ boj/A/2589.cpp
@@ -2,9 +2,14 @@
 using namespace std;
 int w, h;
 char mmap[50][50];
-bool visitied[50][50];
+int visitied[50][50] = {0,};
 int dx[4] = {1,0,-1,0}, dy[4] = {0,1,0,-1};
-int land = 0;
+set<pair<int,int>> land;
+void bfs(){
+    for(int i=0;i<){
+
+    }
+}
 int main(){
     cin >> w >> h;
     for(int i=0;i<h;i++){
@@ -12,9 +17,21 @@ int main(){
         cin >> s;
         for(int j=0;j<w;j++){
             mmap[i][j] = s[j];
-            if(s[j]=='L') land++;
+            if(s[j]=='L') land.insert({i,j});
         }
     }
-
     
+    queue<pair<pair<int,int>, int>> q;
+
+    while(!q.empty()){
+        int dist, x, y;
+        tie(dist, x, y) = 
+        for(int i=0;i<4;i++){
+            int ny = y+dy[i], nx = x+dx[i];
+
+            if(visitied==0){
+                q.
+            }
+        }
+    }
 }
