b0c50d24c7ee883c02e90d5fa780ceffab6a7422
178kg78cm <jhh7550@gmail.com>
2025-03-26 01:56:43 +0900

    20250326

--- boj/greedy/2138.cpp
+++ boj/greedy/2138.cpp
@@ -1,37 +1,51 @@
 #include <iostream>
 #include <string>
+#include <algorithm>
 using namespace std;
 
-int main(){
-    int n;
-    cin >> n;
-    string s, s2;
-    cin >> s;
-    cin >> s2;
+string s, s1, s2;
+int n;
+
+char toggle(char c) {
+    return (c == '0') ? '1' : '0';
+}
+
+int flip(int k){
     int answer = 0;
-    for(int i=0;i<n;i++){
-        if(s[i] != s2[i]){
-            if(i!=0)
-                s[i-1]=='1' ? s[i-1]='0' : s[i-1]='1';
-                
-            s[i]=='1' ? s[i]='0' : s[i]='1';
-            
-            if(i!=n-1)
-                s[i+1]=='1' ? s[i+1]='0' : s[i+1]='1';
+    for(int i = k; i < n; i++){
+        if(s1[i - 1] != s2[i - 1]){
+            if(i - 1 >= 0) s1[i - 1] = toggle(s1[i - 1]);
+            s1[i] = toggle(s1[i]);
+            if(i + 1 < n) s1[i + 1] = toggle(s1[i + 1]);
             answer++;
         }
-
-        int j=0;
-        for(j=0;j<n;j++){
-            if(s[j]!=s2[j]){
-                break;
-            }
-        } if(j==n) {
-            cout << answer;
-            return 0;
-        }
     }
-    
-    cout << -1;
+
+    if(s1 == s2) return answer;
+    else return -1;
+}
+
+int main(){
+    cin >> n >> s >> s2;
+
+    s1 = s;
+    int answer1 = flip(1); // 첫 스위치를 누르지 않은 경우
+
+    s1 = s;
+    if(n >= 1) s1[0] = toggle(s1[0]);
+    if(n >= 2) s1[1] = toggle(s1[1]);
+    int answer2 = flip(1); // 첫 스위치를 누른 경우
+    if(answer2 != -1) answer2 += 1;
+
+    if(answer1 == -1 && answer2 == -1)
+        cout << -1;
+    else if(answer1 == -1)
+        cout << answer2;
+    else if(answer2 == -1)
+        cout << answer1;
+    else
+        cout << min(answer1, answer2);
+
     return 0;
 }
+    
