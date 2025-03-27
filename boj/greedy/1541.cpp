5df37b5ea4147545a90287a170c374816f629262
178kg78cm <jhh7550@gmail.com>
2025-03-28 01:00:50 +0900

    20250328

--- boj/greedy/1541.cpp
+++ boj/greedy/1541.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <algorithm>
 using namespace std;
 
 int main(){
@@ -7,8 +8,33 @@ int main(){
     // -(1 2) -3 -(4 5 6) -(7 8) -9
     string s;
     cin >> s;
-    for(int i=s.size();i>=0;i--){
-        if(s[i]=='+')
+    int minus = 0;
+    string num_str = "";
+    int num = 0;
+    int total = 0;
+    for(int i=s.size()-1;i>=0;i--){
+        if(s[i]=='+'){
+            reverse(num_str.begin(),num_str.end());
+            num += stoi(num_str);
+            num_str = "";
+        }else if(s[i]=='-'){
+            reverse(num_str.begin(),num_str.end());
+            num += stoi(num_str);
+            num_str = "";
+            total -= num;
+            num = 0;
+        }else{
+            num_str.push_back(s[i]);
+        }
     }
     
+    if(num_str != "") {
+        reverse(num_str.begin(),num_str.end());
+        total += stoi(num_str);
+    }if(num!=0){
+        total += num;
+    }
+
+    cout << total;
+    return 0;
 }
