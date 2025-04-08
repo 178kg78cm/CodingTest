db2dc3bdd725014ed1ac360e3cdd225bffea3605
178kg78cm <jhh7550@gmail.com>
2025-04-08 20:39:53 +0900

    20250408

--- boj/A/28215.cpp
+++ boj/A/28215.cpp
@@ -1,18 +1,33 @@
 #include <iostream>
 #include <utility>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
+int N, K;
 
+vector<vector<int>> combination;
+vector<int> current;
+void combi_generate(int start){
+    if(current.size()==K){
+        combination.push_back(current);
+        return;
+    }
+    for(int i=start;i<N;i++){
+        current.push_back(i);
+        combi_generate(i+1);
+        current.pop_back();
+    }
+
+}
 int main(){
-    int N, K;
     cin >> N >> K;
     vector<pair<int,int>> v(N);
-    vector<vector<int>> dist(K, vector<int>(K,0));
+    vector<vector<int>> dist(N, vector<int>(N,0));
     for(int i=0;i<N;i++){
         int x, y;
         cin >> x >> y;
-        v.push_back({x,y});
+        v[i] = {x,y};
     }
     // N to N 거리재기
     for(int i=0;i<N;i++){ // 시작
@@ -22,8 +37,28 @@ int main(){
             }
         }
     }
-
+    combi_generate(0);
     // K개를 골라
-    
+    int num_of_combi = combination.size();
+    vector<int> min_dist;
+    for(int j=0;j<num_of_combi;j++){
+        vector<int> max_dist;
+        for(int k=0;k<N;k++){
+            int min = 1e9;
+            for(int i=0;i<K;i++){
+                int curr = dist[combination[j][i]][k];
+                if(curr<min) min = curr;
+            }
+            max_dist.push_back(min);
+        }
+        min_dist.push_back(*max_element(max_dist.begin(),max_dist.end()));
+    }
 
+    if(min_dist.size()==0) cout << 0;
+    else{
+        cout << *min_element(min_dist.begin(),min_dist.end()); 
+
+    }
+
+    return 0;
 }
